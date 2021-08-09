04c4eff7c6196601d6d2190399cd86e5aea484db
Inna <inna.kachinskaya13@gmail.com>
2021-08-10 01:28:23 +0300

    Fix index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,12 +10,11 @@ class RangeValidator {
       throw new TypeError("Invalid type");
     }
 
-    if (newFrom > this.to || typeof this.to !== "undefined") {
+    if (newFrom > this.to && typeof this.to !== "undefined") {
       throw new RangeError(
         "Начальное значение диапазона не может быть больше конечного"
       );
     }
-
     this._from = newFrom;
   }
 
@@ -28,7 +27,7 @@ class RangeValidator {
       throw new TypeError("Invalid type");
     }
 
-    if (newTo < this.from) {
+    if (newTo < this.from && typeof this.from !== "undefined") {
       throw new RangeError(
         "Конечное значение диапазона не может быть меньше начального"
       );
@@ -40,24 +39,21 @@ class RangeValidator {
     return this._to;
   }
 
-  getterRange() {
-    const array = [];
-    array.push(this.from, this.to);
-    return array;
+  get range() {
+    return [this.from, this.to];
   }
 
   validate(value) {
-    if (isNaN(value)) {
+    if (isNaN(value) || typeof value !== "number") {
       throw new TypeError("Invalid type");
     }
     if (value >= this.from && value <= this.to) {
       return value;
-    } else {
-      throw new RangeError("Число не входит в заданный диапазон");
     }
+    throw new RangeError("Число не входит в заданный диапазон");
   }
 }
 const rangeValidator = new RangeValidator(0, 20);
 console.log(rangeValidator);
-console.log(rangeValidator.getterRange());
+console.log(rangeValidator.range);
 console.log(rangeValidator.validate(15));
